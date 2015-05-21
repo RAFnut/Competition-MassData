f04c10f0de49f4ae2eab98b08f39e47a65512427
Filip Danic <filipdanic@yahoo.com>
2015-05-21 17:21:54 +0200

    js vendors

---
+++ src/AppBundle/Resources/public/js/orchestra.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var O = {
+	version: "Orchestra.js v0.1",
+	sendAjaxRequest: function(endpoint, data, callbackSuccess, callbackError){
+	 	$.ajax({
+	            type: "POST",
+	            url: endpoint,
+	            contentType: "application/json",
+	            dataType: "json",
+	            data: data,
+	            success: function(data) {
+	            	callbackSuccess(data);
+	            },
+	            error: function(data){
+	            	callbackError(data);
+	            }
+	        });
+	}
+}
+
+O.tester1 = function (data){
+	console.log(data);
+}
+O.tester2 = function(data){
+	console.log("Error:" + data);
+}
+
+var RegisterUser = function(email, username, password){
+	this.email = email;
+	this.username = username;
+	this.password = password;
+}
+
+var LoginUser = function(username, password){
+	this.username = username;
+	this.password = password;
+}
