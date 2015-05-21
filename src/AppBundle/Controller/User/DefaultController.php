33cab305123476c485eb6705b05cf66dfcebe973
NikolaKlipa <klipa.nikola@gmail.com>
2015-05-21 17:00:32 +0200

    fica help

--- src/AppBundle/Controller/User/DefaultController.php
+++ src/AppBundle/Controller/User/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
     /**
@@ -14,4 +17,13 @@ class DefaultController extends Controller
     {
         return $this->render('AppBundle:User:index.html.twig');
     }
+
+    /**
+     * @Route("/help", name="fica_help", options={"expose": true})
+     * @Method({"GET", "POST"})
+     */  
+    public function helpAction(Request $request)
+    {
+    	return new Response('OK');
+    }  
 }
