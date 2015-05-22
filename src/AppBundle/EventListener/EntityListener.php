fc9400541d6ae16c2ed41caa777d61205e0bd80c
Andrej Karadzic <karadza3a@yahoo.com>
2015-05-22 11:54:03 +0200

    send mail on user create

--- src/AppBundle/EventListener/EntityListener.php
+++ src/AppBundle/EventListener/EntityListener.php
@@ -3,22 +3,53 @@ namespace AppBundle\EventListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Acme\StoreBundle\Entity\Product;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use AppBundle\Entity\User;
 
 class EntityListener
 {
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function postPersist(LifecycleEventArgs $args)
     {
+        $mailer = $this->container->get('mailer');
+        $templating = $this->container->get('templating');
+
         $entity = $args->getEntity();
         $em = $args->getEntityManager();
 
         // perhaps you only want to act on some "Product" entity
         if ($entity instanceof User) {
-            // ... do something with the Product
-            $entity->setSalt("123431");
-            $em->persist($entity);
-            $em->flush();
+
+            $message = \Swift_Message::newInstance()
+            ->setSubject('Success!')
+            ->setFrom('milance.jovanovic92@gmail.com')
+            ->setTo($entity->getEmail())
+            ->setBody(
+                $templating->render(
+                    'AppBundle:Emails:user-created.html.twig',
+                    array('user' => $entity)
+                ),
+                'text/html'
+            )
+            /*
+             * If you also want to include a plaintext version of the message
+            ->addPart(
+                $this->renderView(
+                    'Emails/registration.txt.twig',
+                    array('name' => $name)
+                ),
+                'text/plain'
+            )
+            */
+            ;
+            $mailer->send($message); 
         }
     }
 }
